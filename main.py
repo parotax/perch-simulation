6bcd87959246f2285c40e1a384dd37ba59c40eee
Luukas Lehtinen <lukituubi@gmail.com>
2022-12-09 14:27:56 +0200

    Keep the fish in the water

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from math import cos, sin
 import random
 
-jarvi = [(0, 0), (1000, 0), (1000, 500), (0, 500)]
+jarvi = 500
 
 class Kala:
     def __init__(self, x, y): 
@@ -10,7 +10,7 @@ class Kala:
         self.y = y
 
 class Parvi:
-    def __init__(self, kalat, vauhti, x, y, suunta):
+    def __init__(self, x, y, kalat, vauhti, suunta, rad):
         self.x = x
         self.y = y
         self.paikatX = [self.x]
@@ -18,13 +18,18 @@ class Parvi:
         self.kalat = kalat
         self.vauhti = vauhti
         self.suunta = suunta
+        self.rad = rad
 
     def liiku(self):
         self.suunta += random.uniform(-1.0 , 1.0)
         
         dx = self.vauhti*sin(self.suunta)
         dy = self.vauhti*cos(self.suunta)
-        
+
+        if self.x + dx <= self.rad or self.y + dy <= self.rad or self.x + dx >= jarvi - self.rad or self.y + dy >= jarvi - self.rad:
+            self.liiku()
+            return
+
         self.x += dx
         self.y += dy
 
@@ -40,14 +45,14 @@ def piirrareitti(xlista, ylista):
     ax.grid()
 
    
-    ax.set_xlim([0-5, 500+5])   # Piirretään symmetrisesti origon ympärille
-    ax.set_ylim([0-5, 1000+5])
+    ax.set_xlim([0-5, jarvi+5])   # Piirretään symmetrisesti origon ympärille
+    ax.set_ylim([0-5, jarvi+5])
     
     plt.show()
 
-kalat = Parvi([], 1, 250, 500, 0)
+kalat = Parvi(jarvi / 2, jarvi / 2, [], 1, 0, 5)
 
-for i in range(20):
+for i in range(10):
     kalat.liiku()
 
 piirrareitti(kalat.paikatX, kalat.paikatY)
