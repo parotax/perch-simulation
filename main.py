2612db5abc51333bb57dc0415195b6c9d6a040c3
keitto <iiris.kilpelainen@gmail.com>
2022-12-09 13:24:03 +0200

    Implement moving parvi

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+from math import cos, sin
+import random
+
 class Kala:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 class Parvi:
-    def __init__(self, kalat, vauhti, x, y):
+    def __init__(self, kalat, vauhti, x, y, suunta):
         self.kalat = kalat
         self.vauhti = vauhti
         self.x = x
         self.y = y
+        self.suunta = suunta
+
+    def liiku(self):
+        self.suunta += random.uniform(-1.0 , 1.0)
+        
+        dx = self.vauhti*sin(self.suunta)
+        dy = self.vauhti*cos(self.suunta)
+        
+        self.x += dx
+        self.y += dy
