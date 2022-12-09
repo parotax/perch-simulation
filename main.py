c57dccd41cecd2179a163c9824034c01697f4448
Luukas Lehtinen <lukituubi@gmail.com>
2022-12-09 13:44:25 +0200

    Add test code and visually show the path of the fish

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import matplotlib.pyplot as plt
 from math import cos, sin
 import random
 
+jarvi = [(0, 0), (1000, 0), (1000, 500), (0, 500)]
+
 class Kala:
-    def __init__(self, x, y):
+    def __init__(self, x, y): 
         self.x = x
         self.y = y
 
 class Parvi:
     def __init__(self, kalat, vauhti, x, y, suunta):
-        self.kalat = kalat
-        self.vauhti = vauhti
         self.x = x
         self.y = y
+        self.paikatX = [self.x]
+        self.paikatY = [self.y]
+        self.kalat = kalat
+        self.vauhti = vauhti
         self.suunta = suunta
 
     def liiku(self):
@@ -22,3 +27,27 @@ class Parvi:
         
         self.x += dx
         self.y += dy
+
+        self.paikatX.append(self.x)
+        self.paikatY.append(self.y)
+
+def piirrareitti(xlista, ylista):
+    
+    fig, ax = plt.subplots()
+    ax.plot(xlista, ylista,'o')
+    ax.set(xlabel='x', ylabel='y',
+    title='Parven reitti')
+    ax.grid()
+
+   
+    ax.set_xlim([0-5, 500+5])   # Piirretään symmetrisesti origon ympärille
+    ax.set_ylim([0-5, 1000+5])
+    
+    plt.show()
+
+kalat = Parvi([], 1, 250, 500, 0)
+
+for i in range(20):
+    kalat.liiku()
+
+piirrareitti(kalat.paikatX, kalat.paikatY)
