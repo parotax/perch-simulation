91a6f3076574101b4323300213a2809c5096a091
Luukas Lehtinen <lukituubi@gmail.com>
2022-12-09 12:58:23 +0200

    Add first classes

---
+++ main.py
@@ -0,0 +1,11 @@
+class Kala:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Parvi:
+    def __init__(self, kalat, vauhti, x, y):
+        self.kalat = kalat
+        self.vauhti = vauhti
+        self.x = x
+        self.y = y
